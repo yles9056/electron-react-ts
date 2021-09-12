0b9c52ab988c6cb8a471dd9ea2494c46b70a2455
AlexQin <qianjin.qin@qq.com>
2021-09-12 20:09:51 +0800

    initial electron project with react in typescript

---
+++ main/preload.ts
@@ -0,0 +1,23 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electron', {
+  ipcRenderer: {
+    myPing() {
+      ipcRenderer.send('ipc-example', 'ping');
+    },
+    on(channel: string, func: (arg0: any) => void) {
+      const validChannels = ['ipc-example'];
+      if (validChannels.includes(channel)) {
+        // Deliberately strip event as it includes `sender`
+        ipcRenderer.on(channel, (event, ...args) => func(args));
+      }
+    },
+    once(channel: string, func: (arg0: any) => void) {
+      const validChannels = ['ipc-example'];
+      if (validChannels.includes(channel)) {
+        // Deliberately strip event as it includes `sender`
+        ipcRenderer.once(channel, (event, ...args) => func(args));
+      }
+    },
+  },
+});
